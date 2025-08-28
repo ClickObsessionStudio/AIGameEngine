3d4efb08195f79e232ca66d1de1aa4c18d368865
Karl Johannes <karl.frjo@gmail.com>
2025-08-29 00:26:09 +0100

    updated index.py path

--- flask-dev/api/index.py
+++ flask-dev/api/index.py
@@ -1,1 +1,8 @@
+import sys
+from pathlib import Path
+
+# Add the parent directory (flask-dev) to the system path
+# This allows Python to find the 'app.py' file
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
+
 from app import app as app
